b601035ee244144fa90de305445f4f43c9182a97
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-10-12 19:31:42 +0300

    1) added tests to test_endpoint.py 2) openai library updated 3) fixed response processing

--- Assignment.py
+++ Assignment.py
@@ -4,7 +4,7 @@ import openai
 from openai import OpenAI
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from sqlalchemy import text
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -21,6 +21,17 @@ class QnA(db.Model):
 
     def __repr__(self):
         return f'QnA {self.id}: {self.question} -> {self.answer}'
+
+
+def create_schema(engine, schema_name):
+    with engine.connect() as connection:
+        result = connection.execute(
+            text(f"SELECT schema_name FROM information_schema.schemata WHERE schema_name = '{schema_name}'")
+        )
+        schema_exists = result.scalar()
+
+        if not schema_exists:
+            connection.execute(text(f'CREATE SCHEMA IF NOT EXISTS {schema_name}'))
 
 
 def create_app():
@@ -57,7 +68,7 @@ def ask():
             ],
             model="gpt-3.5-turbo",
         )
-        answer = response['choices'][0]['message']['content']
+        answer = response.choices[0].message.content
 
         new_entry = QnA(question=question, answer=answer)
         db.session.add(new_entry)
@@ -70,6 +81,7 @@ def ask():
 
 if __name__ == "__main__":
     with app.app_context():
+        create_schema(db.engine, 'questions_answers_schema')
         db.create_all()
 
     app.run(debug=True, port="5000", host='0.0.0.0')
--- test_endpoint.py
+++ test_endpoint.py
@@ -1,1 +1,14 @@
+import json
+import requests
 
+
+def test_ask_endpoint_no_question():
+    response = requests.post('http://127.0.0.1:5000/ask', json={})
+    assert response.status_code == 400
+    json_data = json.loads(response.text)
+    assert json_data['error'] == 'Question not provided'
+
+
+def test_ask_endpoint_with_question():
+    response = requests.post('http://127.0.0.1:5000/ask', json={"question": "What's the capital of Israel?"})
+    assert response.status_code == 200
