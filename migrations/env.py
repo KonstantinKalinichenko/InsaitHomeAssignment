51cbd0a00c6f3219bdbd31647fa0b0976f79176c
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-10-14 22:09:02 +0300

    1) test functions renamed 2) Assignment.py renamed

--- migrations/env.py
+++ migrations/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from Assignment import db
+from flask_server import db
 
 target_metadata = db.metadata
 
